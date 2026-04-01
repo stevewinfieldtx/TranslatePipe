79c589503c9f9f5c6131ca4d7b6eaf8ffdf8e8cb
Steve Winfield <stevewinfieldtx@gmail.com>
2026-04-01 13:25:37 +0700

    simple: /a for English speaker, /b for Spanish speaker

---
+++ app/a/page.js
@@ -0,0 +1,5 @@
+'use client';
+import Translator from '../components/Translator';
+export default function PageA() {
+  return <Translator sourceLang="en" targetLang="es" sourceLabel="English" targetLabel="Spanish" />;
+}
---
+++ app/b/page.js
@@ -0,0 +1,5 @@
+'use client';
+import Translator from '../components/Translator';
+export default function PageB() {
+  return <Translator sourceLang="es" targetLang="en" sourceLabel="Spanish" targetLabel="English" />;
+}
